5c1f0c3c74c3a4021d87e8a78cd038389af37d01
Daguhh <code.daguhh@zaclys.net>
2021-01-28 17:40:23 +0100

    correct bad param shift

--- src/naivecalendar.sh
+++ src/naivecalendar.sh
@@ -25,9 +25,9 @@ if [[ " ${param[@]} " =~ " -t " ]] || [[ " ${param[@]} " =~ " --theme " ]] ; the
         case $key in
             -t|--theme)
             OPT_THEME="$2"
+            shift # past argument
             ;;
         esac
-        shift # past argument
         shift # past value
     done
     THEME="$THEME_USER_PATH/$OPT_THEME).rasi"
