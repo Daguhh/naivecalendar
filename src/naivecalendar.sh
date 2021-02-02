751dcb1815c357f52de93d22cba5b913ec57a14c
Daguhh <code.daguhh@zaclys.net>
2021-02-02 14:24:55 +0100

    only execute -p and -x action once (not in recursions)

--- src/naivecalendar.sh
+++ src/naivecalendar.sh
@@ -3,6 +3,10 @@
 param="$@"
 cmd="${BASH_SOURCE%/*}/naivecalendar.py $param"
 
+##############################################################################
+####################  Process command line arguments #########################
+##############################################################################
+
 # display help msg
 if [[ " ${param[@]} " =~ " -h " ]] || [[ " ${param[@]} " =~ " --help " ]] ; then
     $cmd
@@ -19,6 +23,8 @@ fi
 THEME_CACHE_FILE="$HOME/.cache/naivecalendar/theme_cache.txt"
 THEME_USER_PATH="$HOME/.config/naivecalendar/themes"
 THEME_SOURCE_PATH="${BASH_SOURCE%/*}/themes"
+
+# forced with cmd line argument
 if [[ " ${param[@]} " =~ " -t " ]] || [[ " ${param[@]} " =~ " --theme " ]] ; then
     while [[ $# -gt 0 ]] ; do
         key="$1"
@@ -30,6 +36,7 @@ if [[ " ${param[@]} " =~ " -t " ]] || [[ " ${param[@]} " =~ " --theme " ]] ; the
         esac
         shift # past value
     done
+    # search in user path before
     THEME="$THEME_USER_PATH/$OPT_THEME).rasi"
     if ! test -f "$THEME"; then
         THEME="$THEME_SOURCE_PATH/$OPT_THEME.rasi"
@@ -39,6 +46,7 @@ if [[ " ${param[@]} " =~ " -t " ]] || [[ " ${param[@]} " =~ " --theme " ]] ; the
         exit 0
     fi
 
+# otherwise, look in cache
 elif test -f "$THEME_CACHE_FILE"; then
     # look in user path, then in source path
     THEME="$THEME_USER_PATH/$(cat $THEME_CACHE_FILE).rasi"
@@ -61,10 +69,41 @@ else
     THEME="$THEME_SOURCE_PATH/classic_dark.rasi"
 fi
 
-# launch rofi
-#ROFI_LOG_FILE="$HOME/.cache/naivecalendar/rofi_log.txt"
+##############################################################################
+############################  call rofi  #####################################
+##############################################################################
 
-output="$(rofi -show calendar \
+# Determine if is it a "first" call or a recursion
+if [ -z $NAIVECALENDAR_FIRST_CALL ]; then
+    export NAIVECALENDAR_FIRST_CALL="true"
+fi
+if [ -z $NAIVECALENDAR_SECOND_CALL ]; then
+    export NAIVECALENDAR_SECOND_CALL="true"
+fi
+
+loop_nb=-1
+
+if [[ "$NAIVECALENDAR_FIRST_CALL" == "true" && "$NAIVECALENDAR_SECOND_CALL" == "true" ]]; then
+    loop_nb=0
+    export NAIVECALENDAR_FIRST_CALL="false"
+
+elif [[ "$NAIVECALENDAR_FIRST_CALL" == "false" && "$NAIVECALENDAR_SECOND_CALL" == "true" ]]; then
+    loop_nb=1
+    export NAIVECALENDAR_FIRST_CALL="false"
+    export NAIVECALENDAR_SECOND_CALL="false"
+
+elif [[ "$NAIVECALENDAR_FIRST_CALL" == "false" && "$NAIVECALENDAR_SECOND_CALL" == "false" ]]; then
+    loop_nb=2
+fi
+
+# empty log file at first call
+ROFI_LOG_FILE="$HOME/.cache/naivecalendar/rofi_log.txt"
+if [ $loop_nb -eq 0 ]; then
+    echo "" > "$ROFI_LOG_FILE"
+fi
+
+# launch rofi
+rofi_output="$(rofi -show calendar \
     -modi "calendar:$cmd" \
     -theme-str '@theme "'$THEME'"' \
     -hide-scrollbar true \
@@ -73,19 +112,27 @@ output="$(rofi -show calendar \
     -location 3 \
     2>&1)"
 
-# print date to stdout
-if [[ " ${param[@]} " =~ " -x " ]] || [[ " ${param[@]} " =~ " --clipboard " ]]; then
-    FILE="$HOME/.cache/naivecalendar/pretty_print_cache.txt"
-    cat $FILE | xclip -selection clipboard
+#log
+echo "$rofi_output" >> "$ROFI_LOG_FILE"
+
+##############################################################################
+####################  Naivecalendar ouputs ###################################
+##############################################################################
+
+# execute copy-to-clipboard or print action only with main/first script call
+if [ "$loop_nb" -eq 0 ]; then
+
+    # print date to stdout
+    if [[ " ${param[@]} " =~ " -x " ]] || [[ " ${param[@]} " =~ " --clipboard " ]]; then
+        FILE="$HOME/.cache/naivecalendar/pretty_print_cache.txt"
+        cat $FILE | xclip -selection clipboard &
+    fi
+
+    # print date to stdout
+    if [[ " ${param[@]} " =~ " -p " ]] || [[ " ${param[@]} " =~ " --print " ]]; then
+        FILE="$HOME/.cache/naivecalendar/pretty_print_cache.txt"
+        cat $FILE > "$HOME/temp.txt"
+        printf "%s" "$(cat $FILE)"
+    fi
 fi
-
-# print date to stdout
-if [[ " ${param[@]} " =~ " -p " ]] || [[ " ${param[@]} " =~ " --print " ]]; then
-    FILE="$HOME/.cache/naivecalendar/pretty_print_cache.txt"
-    printf "%s" "$(cat $FILE)"
-fi
-
-
-
-
 
